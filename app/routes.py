5f51f16b6bdbb5aad854c6c7b719e0da87eb5875
AnkitaDey1 <AnkitaDey1@users.noreply.github.com>
2026-05-05 18:21:28 +0530

    Revert "Merge branch 'multiply'"
    This reverts commit abd798b485c67fe064f0f7730b36f924bbdc5e94, reversing
    changes made to d4c8a9f9c79a046157c8cd9b6b0435f61ad7ab3f.

--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,9 @@
-# from fastapi import APIRouter
-# from app.utils import add_numbers
-
-# router = APIRouter()
-
-# @router.get("/add")
-# def add(a: int, b: int):
-#     result = add_numbers(a, b)
-#     return {"result": result}
-
-
 from fastapi import APIRouter
-from app.utils import add_numbers, multiply_numbers, get_history
+from app.utils import add_numbers
 
 router = APIRouter()
 
 @router.get("/add")
 def add(a: int, b: int):
-    return {"result": add_numbers(a, b)}
-
-@router.get("/multiply")
-def multiply(a: int, b: int):
-    return {"result": multiply_numbers(a, b)}
-
-@router.get("/history")
-def history():
-    return {"history": get_history()}
+    result = add_numbers(a, b)
+    return {"result": result}
--- app/utils.py
+++ app/utils.py
@@ -1,18 +1,2 @@
-# def add_numbers(a: int, b: int) -> int:
-#     return a + b
-
-
-history = []
-
 def add_numbers(a: int, b: int) -> int:
-    result = a + b
-    history.append(f"ADD: {a} + {b} = {result}")
-    return result
-
-def multiply_numbers(a: int, b: int) -> int:
-    result = a * b
-    history.append(f"MULTIPLY: {a} * {b} = {result}")
-    return result
-
-def get_history():
-    return history
+    return a + b
